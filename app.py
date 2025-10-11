fb1c47d3d0ba76d912559a2ec05ad0d8949e6e31
KarChikey <kartikeynegi2002@gmail.com>
2025-10-11 10:44:38 +0530

    add

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
-from flask import Flask
+from flask import Flask,jsonify,request
+from flask import SQLALchemy
 
 app=Flask(__name__)
 
-@app.route('/')
-def hello_user():
-    return 'Hello, World!'
+app.config["sqlalchemy_database_uri"]=""
+app.config["sqlalchemy_track_modification"]=False
 
-if __name__=='__main__':
-    app.run(debug=True)
+db=SQLALchemy(app)
+
+class User(db.model):
+    __table__='Employee'
+    id=db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String(100),nullable=False)
+    role=db.Column(db.String(100),nullable=False)
     
+    def to_dict(self):
+        return {"id":self.id,"name":self.name,"role":self.role}
     
+@app.route("/")
+def Home():
+    return "Flask + Backend Running"
+
+@app.route("/users",method=['GET'])
+def get_users():
+    users=User.query
