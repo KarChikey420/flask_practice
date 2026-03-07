afd13d220862359cdd1a4389d3287d33ff5fc38e
KarChikey <kartikeynegi2002@gmail.com>
2026-03-07 22:39:49 +0530

    dd

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 app=Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"]="postgresql://postgres:root@localhost:5432/mydb"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
 
 db=SQLAlchemy(app)
 
