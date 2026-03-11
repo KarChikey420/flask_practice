0cc74abd0420c508123d64e0378a85390726a9db
KarChikey <kartikeynegi2002@gmail.com>
2026-03-11 23:10:13 +0530

    dd

--- program1.py
+++ program1.py
@@ -2,12 +2,6 @@ from fastapi import FastAPI
 import uvicorn
 app=FastAPI()
 
-@app.middleware('http')
-async def middleware(request,call_next):
-    print("Request before calling")
-    response=await call_next(request)
-    print("Request after calling")
-    return response
 
 
 
